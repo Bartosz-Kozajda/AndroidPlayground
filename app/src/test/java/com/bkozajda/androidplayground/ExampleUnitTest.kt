1e214a4be534c275f3287df8d5526486ad707f5a
Bartosz-Kozajda <bartosz.kozajda@gmail.com>
2017-11-05 12:49:47 +0100

    Add ktlint

--- app/src/test/java/com/bkozajda/androidplayground/ExampleUnitTest.kt
+++ app/src/test/java/com/bkozajda/androidplayground/ExampleUnitTest.kt
@@ -2,7 +2,7 @@ package com.bkozajda.androidplayground
 
 import org.junit.Test
 
-import org.junit.Assert.*
+import org.junit.Assert.assertEquals
 
 /**
  * Example local unit test, which will execute on the development machine (host).
