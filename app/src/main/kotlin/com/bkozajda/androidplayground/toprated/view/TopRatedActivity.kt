8bb9250e95042b627f75fe95a4c7aaefca2cb0ab
Bartosz-Kozajda <bartosz.kozajda@gmail.com>
2017-12-18 19:55:22 +0100

    Remove main package

--- app/src/main/kotlin/com/bkozajda/androidplayground/toprated/view/TopRatedActivity.kt
+++ app/src/main/kotlin/com/bkozajda/androidplayground/toprated/view/TopRatedActivity.kt
@@ -1,3 +1,12 @@
 package com.bkozajda.androidplayground.toprated.view
 
-class TopRatedActivity
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import com.bkozajda.androidplayground.R
+
+class TopRatedActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
